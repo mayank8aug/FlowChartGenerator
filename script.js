05153c2737210e2272dc64572b80ed705a92e24c
mayank8aug <37096433+mayank8aug@users.noreply.github.com>
2019-08-12 11:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function init() {
     bindActions();
     initRender();
 
+    // Rendering initial data from storage
     function initRender() {
         let dataItem;
         for (i = 0, len = data.length; i < len; i++) {
@@ -21,11 +22,13 @@ function init() {
         }
     }
 
+    // Dragging element from left sidebar towards the main canvas
     function startDragInitial(event, ui) {
         const draggedEl = ui.helper[0];
         $(draggedEl).addClass('draggable-border');
     }
 
+    // Drop element from left sidebar to the main canvas
     function stopDragInitial(event, ui) {
         let draggedEl = ui.helper[0];
         draggedEl = $(draggedEl);
@@ -45,6 +48,7 @@ function init() {
         createNode(itemType, itemText, top, left);   
     }
 
+    // Creating actual node on the canvas from initial rendering or sidebar drop
     function createNode(itemType, itemText, top, left, index) {
         const el = $("<div></div>").addClass('pos-abs draggable cursor-pointer' + (itemType === 'diamond' ? ' action-decision' : '')).css({ top: top, left: left });
         const elAction = $("<div></div>").addClass(itemType);
@@ -73,6 +77,7 @@ function init() {
         $(elAction).on('click', nodeSelected);
     }
 
+    // Selecting a node
     function nodeSelected() {
         const el = $(this);
         $('.node-selected').removeClass('node-selected');
@@ -80,6 +85,7 @@ function init() {
         $('#label').val(el.text());
     }
 
+    // Updating the node label from the right sidebar
     function updateLabel() {
         const el = $(this);
         const selectedNode = $('.node-selected');
@@ -90,17 +96,20 @@ function init() {
         }
     }
 
+    // Start link creation between two nodes
     function startStroke(event, ui) {
         startStrokeX = ui.offset.left;
         startStrokeY = ui.offset.top;
     }
 
+    // Stretch link creation between two nodes
     function continueStroke(event, ui) {
         const endStrokeX = ui.offset.left;
         const endStrokeY = ui.offset.top;
         linedraw(startStrokeX, startStrokeY, endStrokeX, endStrokeY);
     }
 
+    // End link creation between two nodes
     function endStroke(event, ui) {
         const endStrokeX = ui.offset.left;
         const endStrokeY = ui.offset.top;
@@ -117,6 +126,7 @@ function init() {
         startStrokeY = 0;
     }
 
+    // Creating visual connection between two nodes
     function linedraw(startX, startY, endX, endY) {
         const id = 'line_' + startX + '_' + startY;
         const line = document.getElementById(id);
@@ -134,6 +144,7 @@ function init() {
         $('#droppable').append("<div id='" + id + "' style='transform-origin: 0 100%;width:" + length + "px;height:1px;background-color:black;position:absolute;top:" + (startY) + "px;left:" + (startX) + "px;transform:rotate(" + calc + "deg);-ms-transform:rotate(" + calc + "deg);transform-origin:0% 100%;-moz-transform:rotate(" + calc + "deg);-moz-transform-origin:0% 100%;-webkit-transform:rotate(" + calc + "deg);-webkit-transform-origin:0% 100%;-o-transform:rotate(" + calc + "deg);-o-transform-origin:0% 100%;'></div>");
     }
 
+    // Bind initial actions
     function bindActions() {
         $('.draggable-dummy').draggable({
             delstartY: 300,
@@ -152,7 +163,6 @@ function init() {
                     line && line.remove();
                     return;
                 }
-                debugger;
                 const el = ui.helper && ui.helper[0];
                 if (el) {
                     const id = $(el).attr('id');
